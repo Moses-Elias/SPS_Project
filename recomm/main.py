716a13f43139e03da23e6ca7843917861360da6a
Selma <tunambangula8@gmail.com>
2025-04-13 23:59:46 +0200

    updated
    to the main frontend

--- recomm/main.py
+++ recomm/main.py
@@ -19,11 +19,7 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        
-        "http://localhost:5500",  # Local static HTML server
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],  # Use "*" for testing only, restrict it later for security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
